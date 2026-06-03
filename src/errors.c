34bc998ed11fc3a1173b78a284d525b2a13796ac
Peter Bierma <zintensitydev@gmail.com>
2026-06-03 13:46:55 +0000

    Fix compilation errors.

--- src/errors.c
+++ src/errors.c
@@ -6,14 +6,14 @@ const char *
 UNIT_ErrorCode_ToString(UNIT_ErrorCode code)
 {
     switch (code) {
-#define NAME(name) case name: return #name
-
-        NAME(NO_ERROR);
-        NAME(NO_MEMORY);
-        NAME(INVALID_USAGE);
-        NAME(OS_ERROR);
-
-#undef NAME
+        case UNIT_ERROR_NONE:
+            return "NONE";
+        case UNIT_ERROR_INVALID_USAGE:
+            return "INVALID_USAGE";
+        case UNIT_ERROR_NO_MEMORY:
+            return "NO_MEMORY";
+        case UNIT_ERROR_OS_FAILURE:
+            return "OS_FAILURE";
     }
 }
 
@@ -28,6 +28,6 @@ void
 UNIT_ClearError(UNIT_Context *context)
 {
     assert(context != NULL);
-    context->_internal.error.code = NO_ERROR;
+    context->_internal.error.code = UNIT_ERROR_NONE;
     memset(context->_internal.error.message, 0, 256);
 }
